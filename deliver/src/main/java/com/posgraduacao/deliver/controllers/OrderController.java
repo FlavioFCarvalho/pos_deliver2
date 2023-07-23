1c7df2d2ce237042f9e6b4f0d8013a1e786bca51
FlavioFCarvalho <flaviocfr@gmail.com>
2023-07-23 04:10:17 -0300

    Ordenando os pedidos por ordem pendente e através do ultimo pedido.

--- deliver/src/main/java/com/posgraduacao/deliver/controllers/OrderController.java
+++ deliver/src/main/java/com/posgraduacao/deliver/controllers/OrderController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.posgraduacao.deliver.dtos.OrderDTO;
-import com.posgraduacao.deliver.dtos.ProductDTO;
 import com.posgraduacao.deliver.services.OrderService;
 
 @RestController
